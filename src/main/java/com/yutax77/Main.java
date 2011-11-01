e26d3f679cc2ce28c0718258a61f2087ff6ac672
yutax77 <yutax77@gmail.com>
2011-11-02 00:05:03 +0900

    結果の表示件数を指定可能にした。

--- src/main/java/com/yutax77/Main.java
+++ src/main/java/com/yutax77/Main.java
@@ -7,11 +7,19 @@ import java.util.NavigableSet;
 import java.util.Set;
 
 public class Main {
+	
+	/**
+	 * 結果に表示する順位のデフォルト値
+	 */
+	private static final int DEFAULT_OUTPUT_RANK = 20;
+	
 	public static void main(String[] args) throws IOException {
 		if(args.length < 2) {
-			System.err.println("Usage: java -jar CouncilLottery.jar <ユーザリストファイル名> <ログファイル名>");
+			System.err.println("Usage: java -jar CouncilLottery.jar <ユーザリストファイル名> <ログファイル名> [表示順位]");
 			System.exit(1);
 		}
+		
+		int maxRank = (2 < args.length) ? Integer.valueOf(args[2]) : DEFAULT_OUTPUT_RANK;
 		
 		Set<Person> workers = new WorkerListReader(args[0]).read();
 		Log log = Log.createFromFile(new File(args[1]));
@@ -19,10 +27,10 @@ public class Main {
 		Map<TitleType, Scores> scores = calcurator.calc(log);
 		NavigableSet<Combination> results = CombinationScoreCalculator.calc(scores);
 		
-		outputResult(results);
+		outputResult(results, maxRank);
 	}
 	
-	private static void outputResult(NavigableSet<Combination> results) {
+	private static void outputResult(NavigableSet<Combination> results, int maxRank) {
 		System.out.println("Rank \tScore \t議長 \t書記 \tおやつ係");
 		System.out.println("---------------------------------------------------");
 		int rank = 0;
@@ -33,7 +41,10 @@ public class Main {
 			
 			if(combination.getScore() < score) {
 				rank += count - rank;
+				if(maxRank < rank)
+					break;
 			}
+			
 			score = combination.getScore();
 			buf.append(rank).append("\t");
 			buf.append(String.format("%.2f", score)).append("\t");
