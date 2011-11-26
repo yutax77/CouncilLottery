a378ba159d34faa4c17349cba63a68682f92f919
yutax77 <yutax77@gmail.com>
2011-11-27 02:20:53 +0900

    計算時にスコアの逆順リストになるよう修正。

--- src/main/scala/com/yutax77/councilLottery/Combinations.scala
+++ src/main/scala/com/yutax77/councilLottery/Combinations.scala
@@ -6,17 +6,17 @@ case class Combinations(chairman: (Person, Double), secretary: (Person, Double),
 
 object Combinations {
 	def calc(chairmans: List[(Person, Double)], secretaries: List[(Person, Double)], snacks: List[(Person, Double)]): List[Combinations] = {
-		val descChairmans = chairmans.sortBy(_._2).reverse.groupBy(_._2)
-		val descSecretaries = secretaries.sortBy(_._2).reverse.groupBy(_._2)
-		val descSnacks = snacks.sortBy(_._2).reverse.groupBy(_._2).toList
+		val descChairmans = chairmans.sortBy(_._2).reverse
+		val descSecretaries = secretaries.sortBy(_._2).reverse
+		val descSnacks = snacks.sortBy(_._2).reverse
 		
 		def snackCombinations(chairman: Person, secretary: Person): List[(Set[Person], Double)] = {
 			val pf:PartialFunction[(Person, Double), (Person, Double)] = {
 				case p if p._1 != chairman && p._1 != secretary => p
 			}
 			
-			def makeList(l: List[(Person, Double)], source: List[(Double, List[(Person, Double)])]): List[(Person, Double)] = {
-				val result = l ::: source.head._2.collect(pf)
+			def makeList(l: List[(Person, Double)], source: List[(Person, Double)]): List[(Person, Double)] = {
+				val result = (source.head :: l).collect(pf)
 				if(result.size > 2) {
 					result
 				}
@@ -29,10 +29,8 @@ object Combinations {
 			source.combinations(2).map{l => l.foldLeft(Tuple2(Set.empty[Person], 0.0)){(z, x) => (z._1 + x._1, z._2 + x._2)}}.toList
 		}
 		
-		val result = for(chairmanInfo <- descChairmans;
-			chairman <- chairmanInfo._2;
-			secretaryInfo <- descSecretaries;
-			secretary <- secretaryInfo._2; if secretary != chairman;
+		val result = for(chairman <- descChairmans;
+			secretary <- descSecretaries; if secretary._1 != chairman._1;
 			snacks <- snackCombinations(chairman._1, secretary._1))
 		yield {
 			Combinations(chairman, secretary, snacks)
